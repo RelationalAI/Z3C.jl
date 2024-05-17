5f3cdc3460634c874cd00c309fc68d65bf6f27b4
Remy Wang <remywang@cs.washington.edu>
2024-05-17 16:19:47 -0700

    simplify ctx and references

--- src/Z3.jl
+++ src/Z3.jl
@@ -3,13 +3,15 @@ module Z3
 include("libz3.jl")
 using .Libz3
 import Base: ==
-export DeclareSort, BoolSort, IntSort, BoolVal, IntVal, Solver, add, push, pop, check, CheckResult
+export init_ctx, clear_ctx, DeclareSort, BoolSort, IntSort, BitVecSort, Float16Sort, Float32Sort, Float64Sort,
+BoolVal, IntVal, BitVecVal, Float32Val, Float64Val, 
+FP,
+Solver, del_solver, add, push, pop, check, CheckResult, model
 
 #---------#
 # Context #
 #---------#
 
-# Making struct mutable to register a finalizer
 mutable struct Context
     ctx::Z3_context
 end
@@ -18,28 +20,28 @@ function Context()
     cfg = Z3_mk_config()
     ctx = Z3_mk_context(cfg)
     Z3_del_config(cfg)
-    finalizer(Context(ctx)) do c 
-        Z3_del_context(c.ctx)
-    end
+    return Context(ctx)
 end
 
 # Global Z3 context
 const _main_ctx::Ref{Union{Context,Nothing}} = Ref{Union{Context,Nothing}}(nothing)
 
-function main_ctx()
+function init_ctx()
     global _main_ctx
-    if _main_ctx[] === nothing
-        _main_ctx[] = Context()
-    end
-    return _main_ctx[]
+    @assert isnothing(_main_ctx[]) "Context already initialized"
+    _main_ctx[] = Context()
 end
 
-function _get_ctx(ctx::Union{Context,Nothing})
-    if ctx === nothing
-        return main_ctx().ctx
-    else
-        return ctx.ctx
-    end
+function _get_ctx()
+    @assert !isnothing(_main_ctx[]) "Context not initialized"
+    return _main_ctx[].ctx
+end
+
+function clear_ctx()
+    global _main_ctx
+    @assert !isnothing(_main_ctx[]) "Context not initialized"
+    Z3_del_context(_main_ctx[].ctx)
+    _main_ctx[] = nothing
 end
 
 #-----#
@@ -48,87 +50,91 @@ end
 
 abstract type AST end
 
-Base.show(io::IO, x::AST) = print(io, unsafe_string(Z3_ast_to_string(ctx(x), as_ast(x))))
+Base.show(io::IO, x::AST) = print(io, unsafe_string(Z3_ast_to_string(_get_ctx(), as_ast(x))))
 
 #-------#
 # Sorts #
 #-------#
 
 struct Sort <: AST
-    ctx::Z3_context
     sort::Z3_sort
 end
 
-function Sort(s::Z3_sort, ctx=nothing)
-    c = _get_ctx(ctx)
-    return Sort(c, s)
-end
+as_ast(s::Sort) = Z3_sort_to_ast(_get_ctx(), s.sort)
 
-ctx(s::Sort) = s.ctx
-
-as_ast(s::Sort) = Z3_sort_to_ast(ctx(s), s.sort)
-
-function DeclareSort(name::Union{String,Int}, ctx=nothing)
-    sym = to_symbol(name, ctx)
-    sort = Z3_mk_uninterpreted_sort(_get_ctx(ctx), sym)
+function DeclareSort(name::Union{String,Int})
+    sym = to_symbol(name)
+    sort = Z3_mk_uninterpreted_sort(_get_ctx(), sym)
     return Sort(sort)
 end
 
-BoolSort(ctx=nothing) = Sort(Z3_mk_bool_sort(_get_ctx(ctx)))
-IntSort(ctx=nothing) = Sort(Z3_mk_int_sort(_get_ctx(ctx)))
+BoolSort() = Sort(Z3_mk_bool_sort(_get_ctx()))
+IntSort() = Sort(Z3_mk_int_sort(_get_ctx()))
+BitVecSort(sz::Int) = Sort(Z3_mk_bv_sort(_get_ctx(), sz))
+Float16Sort() = Sort(Z3_mk_fpa_sort_16(_get_ctx()))
+Float32Sort() = Sort(Z3_mk_fpa_sort_32(_get_ctx()))
+Float64Sort() = Sort(Z3_mk_fpa_sort_64(_get_ctx()))
 
 #-------------#
 # Expressions #
 #-------------#
 
 struct Expr <: AST
-    ctx::Z3_context
     expr::Z3_ast
 end
 
-function Expr(expr::Z3_ast, ctx=nothing)
-    c = _get_ctx(ctx)
-    return Expr(c, expr)
-end
-
-ctx(e::Expr) = e.ctx
-
 as_ast(e::Expr) = e.expr
 
-BoolVal(b::Bool, ctx=nothing) = Expr(b ? Z3_mk_true(_get_ctx(ctx)) : Z3_mk_false(_get_ctx(ctx)))
+BoolVal(b::Bool) = Expr(b ? Z3_mk_true(_get_ctx()) : Z3_mk_false(_get_ctx()))
 
-IntVal(n::Integer, ctx=nothing) = Expr(Z3_mk_numeral(_get_ctx(ctx), string(n), IntSort(ctx).sort))
+IntVal(n::Integer) = Expr(Z3_mk_numeral(_get_ctx(), string(n), IntSort().sort))
 
-(==)(a::Expr, b::Expr) = Expr(Z3_mk_eq(ctx(a), as_ast(a), as_ast(b)))
+BitVecVal(v::Integer, sz::Int) = Expr(Z3_mk_numeral(_get_ctx(), string(v), BitVecSort(sz).sort))
 
+# Float16Val(v::Float16) = Expr(Z3_mk_fpa_numeral_float(_get_ctx(), v, Float16Sort().sort))
+Float32Val(v::Float32) = Expr(Z3_mk_fpa_numeral_float(_get_ctx(), v, Float32Sort().sort))
+Float64Val(v::Float64) = Expr(Z3_mk_fpa_numeral_double(_get_ctx(), v, Float64Sort().sort))
+
+FP(name::String, fpsort::Sort) = Expr(Z3_mk_const(_get_ctx(), to_symbol(name), fpsort.sort))
+
+(==)(a::Expr, b::Expr) = Expr(Z3_mk_eq(_get_ctx(), as_ast(a), as_ast(b)))
 
 #--------#
 # Solver #
 #--------#
 
 mutable struct Solver
-    ctx::Z3_context
     solver::Z3_solver
 end
 
-function Solver(ctx=nothing)
-    c = _get_ctx(ctx)
-    s = Z3_mk_solver(c)
-    finalizer(Solver(c, s)) do s
-        Z3_dec_ref(s.ctx, s.solver)
-    end
+function Solver()
+    Solver(Z3_mk_solver(_get_ctx()))
+end
+
+function del_solver(s::Solver)
+    Z3_solver_dec_ref(_get_ctx(), s.solver)
 end
 
 function add(s::Solver, e::Expr)
-    Z3_solver_assert(s.ctx, s.solver, as_ast(e))
+    Z3_solver_assert(_get_ctx(), s.solver, as_ast(e))
 end
 
 function push(s::Solver)
-    Z3_solver_push(s.ctx, s.solver)
+    Z3_solver_push(_get_ctx(), s.solver)
 end
 
 function pop(s::Solver, n=1)
-    Z3_solver_pop(s.ctx, s.solver, n)
+    Z3_solver_pop(_get_ctx(), s.solver, n)
+end
+
+struct Model
+    model::Z3_model
+end
+
+model(s::Solver) = Model(Z3_solver_get_model(_get_ctx(), s.solver))
+
+function Base.show(io::IO, m::Model)
+    print(io, unsafe_string(Z3_model_to_string(_get_ctx(), m.model)))
 end
 
 struct CheckResult
@@ -141,15 +147,15 @@ function Base.show(io::IO, r::CheckResult)
     print(io, r.result == Z3_L_TRUE ? "sat" : r.result == Z3_L_FALSE ? "unsat" : "unknown")
 end
 
-check(s::Solver) = CheckResult(Z3_solver_check(s.ctx, s.solver))
+check(s::Solver) = CheckResult(Z3_solver_check(_get_ctx(), s.solver))
 
 #--------#
 # Others #
 #--------#
 
-function to_symbol(s::Union{String,Int}, ctx=nothing)
-    _sym(s::String) = Z3_mk_string_symbol(_get_ctx(ctx), s)
-    _sym(s::Int) = Z3_mk_int_symbol(_get_ctx(ctx), s)
+function to_symbol(s::Union{String,Int})
+    _sym(s::String) = Z3_mk_string_symbol(_get_ctx(), s)
+    _sym(s::Int) = Z3_mk_int_symbol(_get_ctx(), s)
     return _sym(s)
 end
 
