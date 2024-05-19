778eda278b16a87c8673abbf4da81354b8e08036
Remy Wang <remywang@cs.washington.edu>
2024-05-19 13:48:53 -0700

    Use reference counts

--- src/Z3.jl
+++ src/Z3.jl
@@ -18,30 +18,35 @@ end
 
 function Context()
     cfg = Z3_mk_config()
-    ctx = Z3_mk_context(cfg)
+    ctx = Z3_mk_context_rc(cfg)
+    c = Context(ctx)
     Z3_del_config(cfg)
-    return Context(ctx)
+    finalizer(c) do c
+        Z3_del_context(c.ctx)
+    end
+end
+
+function ref(c::Context)
+    c.ctx
 end
 
 # Global Z3 context
 const _main_ctx::Ref{Union{Context,Nothing}} = Ref{Union{Context,Nothing}}(nothing)
 
-function init_ctx()
+function main_ctx()
     global _main_ctx
-    @assert isnothing(_main_ctx[]) "Context already initialized"
-    _main_ctx[] = Context()
+    if isnothing(_main_ctx[])
+        _main_ctx[] = Context()
+    end
+    return _main_ctx[]
 end
 
-function _get_ctx()
-    @assert !isnothing(_main_ctx[]) "Context not initialized"
-    return _main_ctx[].ctx
-end
-
-function clear_ctx()
-    global _main_ctx
-    @assert !isnothing(_main_ctx[]) "Context not initialized"
-    Z3_del_context(_main_ctx[].ctx)
-    _main_ctx[] = nothing
+function _get_ctx(ctx::Union{Context,Nothing})
+    if isnothing(ctx)
+        return main_ctx()
+    else
+        return ctx
+    end
 end
 
 #-----#
@@ -50,112 +55,139 @@ end
 
 abstract type AST end
 
-Base.show(io::IO, x::AST) = print(io, unsafe_string(Z3_ast_to_string(_get_ctx(), as_ast(x))))
+Base.show(io::IO, x::AST) = print(io, unsafe_string(Z3_ast_to_string(ctx_ref(x), as_ast(x))))
 
 #-------#
 # Sorts #
 #-------#
 
 struct Sort <: AST
-    sort::Z3_sort
+    ctx::Context
+    ast::Z3_sort
 end
 
-as_ast(s::Sort) = Z3_sort_to_ast(_get_ctx(), s.sort)
+as_ast(s::Sort) = Z3_sort_to_ast(ctx_ref(s), s.ast)
+ctx_ref(s::Sort) = ref(s.ctx)
 
-function DeclareSort(name::Union{String,Int})
-    sym = to_symbol(name)
-    sort = Z3_mk_uninterpreted_sort(_get_ctx(), sym)
-    return Sort(sort)
+function DeclareSort(name::Union{String,Int}, ctx=nothing)
+    ctx = _get_ctx(ctx)
+    sym = to_symbol(name, ctx)
+    sort = Z3_mk_uninterpreted_sort(ref(ctx), sym)
+    return Sort(ctx, sort)
 end
 
-BoolSort() = Sort(Z3_mk_bool_sort(_get_ctx()))
-IntSort() = Sort(Z3_mk_int_sort(_get_ctx()))
-BitVecSort(sz::Int) = Sort(Z3_mk_bv_sort(_get_ctx(), sz))
-Float16Sort() = Sort(Z3_mk_fpa_sort_16(_get_ctx()))
-Float32Sort() = Sort(Z3_mk_fpa_sort_32(_get_ctx()))
-Float64Sort() = Sort(Z3_mk_fpa_sort_64(_get_ctx()))
+function BoolSort(ctx=nothing)
+    ctx = _get_ctx(ctx)
+    Sort(ctx, Z3_mk_bool_sort(ref(ctx)))
+end
+
+function IntSort(ctx=nothing)
+    ctx = _get_ctx(ctx)
+    Sort(ctx, Z3_mk_int_sort(ref(ctx)))
+end
+
+function BitVecSort(sz::Int, ctx=nothing)
+    ctx = _get_ctx(ctx)
+    Sort(ctx, Z3_mk_bv_sort(ref(ctx), sz))
+end
+
+function Float16Sort(ctx=nothing)
+    ctx = _get_ctx(ctx)
+    Sort(ctx, Z3_mk_fpa_sort_16(ref(ctx)))
+end
+
+function Float32Sort(ctx=nothing)
+    ctx = _get_ctx(ctx)
+    Sort(ctx, Z3_mk_fpa_sort_32(ref(ctx)))
+end
+
+function Float64Sort(ctx=nothing)
+    ctx = _get_ctx(ctx)
+    Sort(ctx, Z3_mk_fpa_sort_64(ref(ctx)))
+end
 
 #-------------#
 # Expressions #
 #-------------#
 
-struct Expr <: AST
-    expr::Z3_ast
-end
+# struct Expr <: AST
+#     expr::Z3_ast
+# end
 
-as_ast(e::Expr) = e.expr
+# as_ast(e::Expr) = e.expr
 
-BoolVal(b::Bool) = Expr(b ? Z3_mk_true(_get_ctx()) : Z3_mk_false(_get_ctx()))
+# BoolVal(b::Bool) = Expr(b ? Z3_mk_true(_get_ctx()) : Z3_mk_false(_get_ctx()))
 
-IntVal(n::Integer) = Expr(Z3_mk_numeral(_get_ctx(), string(n), IntSort().sort))
+# IntVal(n::Integer) = Expr(Z3_mk_numeral(_get_ctx(), string(n), IntSort().sort))
 
-BitVecVal(v::Integer, sz::Int) = Expr(Z3_mk_numeral(_get_ctx(), string(v), BitVecSort(sz).sort))
+# BitVecVal(v::Integer, sz::Int) = Expr(Z3_mk_numeral(_get_ctx(), string(v), BitVecSort(sz).sort))
 
-# Float16Val(v::Float16) = Expr(Z3_mk_fpa_numeral_float(_get_ctx(), v, Float16Sort().sort))
-Float32Val(v::Float32) = Expr(Z3_mk_fpa_numeral_float(_get_ctx(), v, Float32Sort().sort))
-Float64Val(v::Float64) = Expr(Z3_mk_fpa_numeral_double(_get_ctx(), v, Float64Sort().sort))
+# # Float16Val(v::Float16) = Expr(Z3_mk_fpa_numeral_float(_get_ctx(), v, Float16Sort().sort))
+# Float32Val(v::Float32) = Expr(Z3_mk_fpa_numeral_float(_get_ctx(), v, Float32Sort().sort))
+# Float64Val(v::Float64) = Expr(Z3_mk_fpa_numeral_double(_get_ctx(), v, Float64Sort().sort))
 
-FP(name::String, fpsort::Sort) = Expr(Z3_mk_const(_get_ctx(), to_symbol(name), fpsort.sort))
+# FP(name::String, fpsort::Sort) = Expr(Z3_mk_const(_get_ctx(), to_symbol(name), fpsort.sort))
 
-(==)(a::Expr, b::Expr) = Expr(Z3_mk_eq(_get_ctx(), as_ast(a), as_ast(b)))
+# (==)(a::Expr, b::Expr) = Expr(Z3_mk_eq(_get_ctx(), as_ast(a), as_ast(b)))
 
 #--------#
 # Solver #
 #--------#
 
-mutable struct Solver
-    solver::Z3_solver
-end
+# mutable struct Solver
+#     solver::Z3_solver
+# end
 
-function Solver()
-    Solver(Z3_mk_solver(_get_ctx()))
-end
+# function Solver()
+#     Solver(Z3_mk_solver(_get_ctx()))
+# end
 
-function del_solver(s::Solver)
-    Z3_solver_dec_ref(_get_ctx(), s.solver)
-end
+# function del_solver(s::Solver)
+#     Z3_solver_dec_ref(_get_ctx(), s.solver)
+# end
 
-function add(s::Solver, e::Expr)
-    Z3_solver_assert(_get_ctx(), s.solver, as_ast(e))
-end
+# function add(s::Solver, e::Expr)
+#     Z3_solver_assert(_get_ctx(), s.solver, as_ast(e))
+# end
 
-function push(s::Solver)
-    Z3_solver_push(_get_ctx(), s.solver)
-end
+# function push(s::Solver)
+#     Z3_solver_push(_get_ctx(), s.solver)
+# end
 
-function pop(s::Solver, n=1)
-    Z3_solver_pop(_get_ctx(), s.solver, n)
-end
+# function pop(s::Solver, n=1)
+#     Z3_solver_pop(_get_ctx(), s.solver, n)
+# end
 
-struct Model
-    model::Z3_model
-end
+# struct Model
+#     model::Z3_model
+# end
 
-model(s::Solver) = Model(Z3_solver_get_model(_get_ctx(), s.solver))
+# model(s::Solver) = Model(Z3_solver_get_model(_get_ctx(), s.solver))
 
-function Base.show(io::IO, m::Model)
-    print(io, unsafe_string(Z3_model_to_string(_get_ctx(), m.model)))
-end
+# function Base.show(io::IO, m::Model)
+#     print(io, unsafe_string(Z3_model_to_string(_get_ctx(), m.model)))
+# end
 
-struct CheckResult
-    result::Z3_lbool
-end
+# struct CheckResult
+#     result::Z3_lbool
+# end
 
-CheckResult(r::Symbol) = r == :sat ? CheckResult(Z3_L_TRUE) : r == :unsat ? CheckResult(Z3_L_FALSE) : CheckResult(Z3_L_UNDEF) 
+# CheckResult(r::Symbol) = r == :sat ? CheckResult(Z3_L_TRUE) : r == :unsat ? CheckResult(Z3_L_FALSE) : CheckResult(Z3_L_UNDEF) 
 
-function Base.show(io::IO, r::CheckResult)
-    print(io, r.result == Z3_L_TRUE ? "sat" : r.result == Z3_L_FALSE ? "unsat" : "unknown")
-end
+# function Base.show(io::IO, r::CheckResult)
+#     print(io, r.result == Z3_L_TRUE ? "sat" : r.result == Z3_L_FALSE ? "unsat" : "unknown")
+# end
 
-check(s::Solver) = CheckResult(Z3_solver_check(_get_ctx(), s.solver))
+# check(s::Solver) = CheckResult(Z3_solver_check(_get_ctx(), s.solver))
 
 #--------#
 # Others #
 #--------#
 
-function to_symbol(s::Union{String,Int})
-    _sym(s::String) = Z3_mk_string_symbol(_get_ctx(), s)
-    _sym(s::Int) = Z3_mk_int_symbol(_get_ctx(), s)
+function to_symbol(s::Union{String,Int}, ctx=nothing)
+    c = ref(_get_ctx(ctx))
+    _sym(s::String) = Z3_mk_string_symbol(c, s)
+    _sym(s::Int) = Z3_mk_int_symbol(c, s)
     return _sym(s)
 end
 
--- test/Z3_tests.jl
+++ test/Z3_tests.jl
@@ -1,104 +1,96 @@
 @testitem "create sort" begin
-    init_ctx()
     sort = DeclareSort("S")
     @test "$sort" == "S"
-    clear_ctx()
 end
 
 @testitem "create bool sort" begin
-    init_ctx()
     sort = BoolSort()
     @test "$sort" == "Bool"
-    clear_ctx()
 end
 
-@testitem "create bool value" begin
-    init_ctx()
-    t = BoolVal(true)
-    @test "$t" == "true"
-    f = BoolVal(false)
-    @test "$f" == "false"
-    clear_ctx()
-end
+# @testitem "create bool value" begin
+#     init_ctx()
+#     t = BoolVal(true)
+#     @test "$t" == "true"
+#     f = BoolVal(false)
+#     @test "$f" == "false"
+#     clear_ctx()
+# end
 
-@testitem "simple solve" begin
-    init_ctx()
-    s = Solver()
-    push(s)
-    add(s, BoolVal(false))
-    r = check(s)
-    @test r == CheckResult(:unsat)
-    pop(s)
-    r = check(s)
-    @test r == CheckResult(:sat)
-    add(s, BoolVal(true))
-    @test check(s) == CheckResult(:sat)
-    # del_solver(s)
-    clear_ctx()
-end
+# @testitem "simple solve" begin
+#     init_ctx()
+#     s = Solver()
+#     push(s)
+#     add(s, BoolVal(false))
+#     r = check(s)
+#     @test r == CheckResult(:unsat)
+#     pop(s)
+#     r = check(s)
+#     @test r == CheckResult(:sat)
+#     add(s, BoolVal(true))
+#     @test check(s) == CheckResult(:sat)
+#     # del_solver(s)
+#     clear_ctx()
+# end
 
 @testitem "create int sort" begin
-    init_ctx()
     sort = IntSort()
     @test "$sort" == "Int"
-    clear_ctx()
 end
 
-@testitem "create int value" begin
-    init_ctx()
-    n = IntVal(42)
-    @test "$n" == "42"
-    clear_ctx()
-end
+# @testitem "create int value" begin
+#     init_ctx()
+#     n = IntVal(42)
+#     @test "$n" == "42"
+#     clear_ctx()
+# end
 
-@testitem "int equality" begin
-    init_ctx()
-    s = Solver()
-    n1 = IntVal(42)
-    n2 = IntVal(42)
-    add(s, n1 == n2)
-    r = check(s)
-    @test r == CheckResult(:sat)
-    n3 = IntVal(43)
-    add(s, n1 == n3)
-    r = check(s)
-    @test r == CheckResult(:unsat)
-    # del_solver(s)
-    clear_ctx()
-end
+# @testitem "int equality" begin
+#     init_ctx()
+#     s = Solver()
+#     n1 = IntVal(42)
+#     n2 = IntVal(42)
+#     add(s, n1 == n2)
+#     r = check(s)
+#     @test r == CheckResult(:sat)
+#     n3 = IntVal(43)
+#     add(s, n1 == n3)
+#     r = check(s)
+#     @test r == CheckResult(:unsat)
+#     # del_solver(s)
+#     clear_ctx()
+# end
 
-@testitem "create bitvec sort" begin
-    init_ctx()
-    sort = BitVecSort(8)
-    @test "$sort" == "(_ BitVec 8)"
-    clear_ctx()
-end
+# @testitem "create bitvec sort" begin
+#     init_ctx()
+#     sort = BitVecSort(8)
+#     @test "$sort" == "(_ BitVec 8)"
+#     clear_ctx()
+# end
 
-@testitem "create bitvec value" begin
-    init_ctx()
-    v = BitVecVal(42, 8)
-    @test "$v" == "#x2a"
-    clear_ctx()
-end
+# @testitem "create bitvec value" begin
+#     init_ctx()
+#     v = BitVecVal(42, 8)
+#     @test "$v" == "#x2a"
+#     clear_ctx()
+# end
 
 @testitem "create float sorts" begin
-    init_ctx()
     sort = Float16Sort()
     @test "$sort" == "(_ FloatingPoint 5 11)"
     sort = Float32Sort()
     @test "$sort" == "(_ FloatingPoint 8 24)"
     sort = Float64Sort()
     @test "$sort" == "(_ FloatingPoint 11 53)"
-    clear_ctx()
 end
 
-@testitem "create float values" begin
-    init_ctx()
-    v = Float32Val(Float32(3.14))
-    # hex & binary representation of 3.14
-    @test "$v" == "(fp #b0 #x80 #b10010001111010111000011)"
-    v = Float64Val(Float64(3.14))
-    # binary & hex representation of 3.14
-    @test "$v" == "(fp #b0 #b10000000000 #x91eb851eb851f)"
-    clear_ctx()
-end
+# @testitem "create float values" begin
+#     init_ctx()
+#     v = Float32Val(Float32(3.14))
+#     # hex & binary representation of 3.14
+#     @test "$v" == "(fp #b0 #x80 #b10010001111010111000011)"
+#     v = Float64Val(Float64(3.14))
+#     # binary & hex representation of 3.14
+#     @test "$v" == "(fp #b0 #b10000000000 #x91eb851eb851f)"
+#     clear_ctx()
+# end
